2b251c1098d5874ce6158b61bcad30cb50c3d91a
Sebastian LaVine <mail@smlavine.com>
2021-12-26 00:40:32 -0500

    Replace references to navipage

--- err.c
+++ err.c
@@ -1,15 +1,13 @@
 /*
- * navipage - multi-file pager for watching YouTube videos
+ * err - Small error-printing library
  * Copyright (C) 2021 Sebastian LaVine <mail@smlavine.com>
  *
- * This file is part of navipage.
- *
- * navipage is free software: you can redistribute it and/or modify
+ * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
  *
- * navipage is distributed in the hope that it will be useful,
+ * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
  * GNU General Public License for more details.
